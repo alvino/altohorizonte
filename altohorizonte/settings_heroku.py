942f3652f62bce0c7cabe85fe2529325329a8ff6
Alvino Figueiredo <crazyduckalvino@gmail>
2019-01-22 08:58:46 -0200

    alterando setting

--- altohorizonte/settings_heroku.py
+++ altohorizonte/settings_heroku.py
@@ -91,3 +91,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "imagens")
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
